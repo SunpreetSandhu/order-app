688314cc5031006803788d0ae94f5c7ecde2a1d5
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-22 15:04:34 -0400

    declared routes imparative way using createbrowserrouter and an external router to later enable data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+/*
+imparative way to declare routes outside jsx
+old way:  <browser router> < routes> <route path = 'menu'> element = <menu/> />
+nessecary to enable data fetching w react router
+*/
 const router = createBrowserRouter([
   {
     path: "/",
@@ -10,7 +19,20 @@ const router = createBrowserRouter([
     path: "/menu",
     element: <Menu />,
   },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
 ]);
+
 function App() {
   return <RouterProvider router={router} />;
 }
