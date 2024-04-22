02686bfa1b258339bf7dc4b7362b7ca5962bcc98
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-22 18:28:44 -0400

    added outlet to display child routes when navigated to in applayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 function AppLayout() {
@@ -6,6 +7,10 @@ function AppLayout() {
       <Header />
       <main>
         <h1>Content</h1>
+        {/* This is to display child routes 
+        when they are navigated to like /, /menu, etc
+        */}
+        <Outlet />
       </main>
       <CartOverview />
     </div>
