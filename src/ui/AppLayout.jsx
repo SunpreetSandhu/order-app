0c37ce9f19237221223f1aaccdf39afdee92b3e3
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-25 12:53:04 -0400

    rendered loader using usenavigation state and loader component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,19 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  console.log(navigation);
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <h1>Content</h1>
         {/* This is to display child routes 
-        when they are navigated to like /, /menu, etc
+        when they are navigated to like /, /menu, etc from appjsx
         */}
         <Outlet />
       </main>
