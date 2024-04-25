5c427f03c7c4aeb5c20dbf4a36dff6cee23ac6a6
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-25 15:29:19 -0400

    redirected to orderid url once a new order is submitted

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Form } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -87,7 +88,10 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   }; //data in shape we want
-  return null;
+  const newOrder = await createOrder(order);
+
+  //cant use navigate since we not in component
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
