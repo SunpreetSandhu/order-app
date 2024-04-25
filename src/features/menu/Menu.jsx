6c9fbd194919c0b3f6159dbc6cf37cc257998ddb
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-25 12:38:41 -0400

    rendered menuitem for each pizza fetched from api

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,24 @@
+import { useLoaderData } from "react-router";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  //gets data into component from loader defined in routes
+  //fetches as it mounts at same time
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
+//loader function to get data from api
+//getmenu from apiresteraunt file fetches api menu and returns data.json
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
 export default Menu;
