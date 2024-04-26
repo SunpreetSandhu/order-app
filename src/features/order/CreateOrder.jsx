9dcf957396c4ffaad4bebc8dc2df4a5ef1629fa9
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-25 20:02:43 -0400

    displayed phone error message

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect, useNavigation } from "react-router-dom";
+import { Form, redirect, useNavigation, useActionData } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -35,6 +35,7 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+  const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -53,6 +54,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -92,6 +94,16 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   }; //data in shape we want
+
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone = "Give a correct phone number please.";
+  }
+  if (Object.keys(errors).length > 0) {
+    return errors;
+  }
+
+  //if everything is ok create new order and redirect
   const newOrder = await createOrder(order);
 
   //cant use navigate since we not in component
