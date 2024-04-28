6e67959a2d3f53125f7f97ef361c28246e2ee140
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-28 17:36:36 -0400

    added more responsive design and header flexbox

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,17 +1,16 @@
-import { Outlet, useNavigation } from "react-router";
-import CartOverview from "../features/cart/CartOverview";
-import Header from "./Header";
-import Loader from "./Loader";
+import { Outlet, useNavigation } from 'react-router';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+import Loader from './Loader';
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
   console.log(navigation);
   return (
     <div className="layout">
       {isLoading && <Loader />}
       <Header />
       <main>
-        <h1>Content</h1>
         {/* This is to display child routes 
         when they are navigated to like /, /menu, etc from appjsx
         */}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from '../features/user/Username';
 function Header() {
   return (
     <header
-      className="px border-b border-stone-200
+      className="flex items-center justify-between border-b border-stone-200
      bg-teal-600 px-4 py-3 uppercase sm:px-6"
     >
       <Link to="/" className="tracking-widest">
