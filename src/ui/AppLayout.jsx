9b58302b1f862f8ca4ab249bdcc2d5e4b2caee9a
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-29 18:02:34 -0400

    positioned loader in center of screen

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,7 +8,9 @@ function AppLayout() {
   console.log(navigation);
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      {isLoading && <Loader />}
+      {/* {isLoading && <Loader />} */}
+      {true && <Loader />}
+
       <Header />
       <div className="overflow-scroll">
         <main className="mx-auto max-w-3xl">
