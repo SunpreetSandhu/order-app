cb24682254f454a8085aba5e05d8c58cbd8e050b
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-29 12:45:45 -0400

    added grid layout to applayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -7,7 +7,7 @@ function AppLayout() {
   const isLoading = navigation.state === 'loading';
   console.log(navigation);
   return (
-    <div className="grid">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
       <main>
