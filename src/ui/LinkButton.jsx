cc9a6e66122f31caa3ee55e8229218b572176ef3
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-29 17:51:15 -0400

    created navigate back function within linkbutton component

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,8 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+  const className = 'text-gray-500-500 text-sm hover:text-blue-600';
+  if (to === '-1')
+    return (
+      <button className={className} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
   return (
-    <Link to={to} className="text-gray-500-500 text-sm hover:text-blue-600">
+    <Link to={to} className={className}>
       {children}
     </Link>
   );
