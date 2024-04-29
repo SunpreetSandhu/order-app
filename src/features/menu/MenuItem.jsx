7705fe42cee0f339efd9ecc061eb7e5a33500e21
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-29 18:39:51 -0400

    made image gray if sold out

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -5,7 +5,11 @@ function MenuItem({ pizza }) {
 
   return (
     <li className="flex gap-4 py-2">
-      <img src={imageUrl} alt={name} className="h-24" />
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+      />
       <div className="flex flex-col">
         <p>{name}</p>
         <p>{ingredients.join(', ')}</p>
