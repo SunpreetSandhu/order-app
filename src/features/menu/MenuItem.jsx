1933dfc93553874099be389bf0eb7963f0dcc7e8
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-29 18:33:45 -0400

    displayed price at bottom of desc

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -6,10 +6,10 @@ function MenuItem({ pizza }) {
   return (
     <li className="flex gap-4 py-2">
       <img src={imageUrl} alt={name} className="h-24" />
-      <div>
+      <div className="flex flex-col">
         <p>{name}</p>
         <p>{ingredients.join(', ')}</p>
-        <div>
+        <div className="mt-auto">
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>
       </div>
