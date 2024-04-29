578ddb8ec534dbb4e399b5fa549b4c1379dab7e2
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-29 17:29:35 -0400

    created button component to encapsulate styles to be reused

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Form, redirect, useNavigation, useActionData } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
+import Button from '../../ui/Button';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -46,13 +47,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input" />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -82,14 +83,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className="inline-block rounded-full bg-teal-500 px-4 py-3 
-            font-semibold uppercase tracking-wide text-stone-800 
-            transition-colors duration-300 hover:bg-teal-300 focus:bg-teal-300 focus:outline-none focus:ring focus:ring-teal-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? 'Placing order...' : 'Order now'}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
---
+++ src/ui/Button.jsx
@@ -0,0 +1,14 @@
+function Button({ children, disabled }) {
+  return (
+    <button
+      disabled={disabled}
+      className="inline-block rounded-full bg-teal-500 px-4 py-3 
+            font-semibold uppercase tracking-wide text-stone-800 
+            transition-colors duration-300 hover:bg-teal-300 focus:bg-teal-300 focus:outline-none focus:ring focus:ring-teal-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
