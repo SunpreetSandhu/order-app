e8500c87be387b3b43b38a3bdc43bafb1f987483
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-30 14:53:23 -0400

    exported action creators and carslice reducer

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -42,3 +42,13 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
