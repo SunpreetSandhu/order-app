55984db0613bc0d6732aaf3aecfb3b1d3d108fec
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-05-02 13:25:38 -0400

    increased item quantity and decreased based on click

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,10 +1,23 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
+import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
 
 function UpdateItemQuantity({ pizzaId }) {
+  const dispatch = useDispatch();
   return (
-    <div>
-      <Button type="round">-</Button>
-      <Button type="round">+</Button>
+    <div className="flex items-center gap-1 md:gap-3">
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
+        -
+      </Button>
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
+      </Button>
     </div>
   );
 }
